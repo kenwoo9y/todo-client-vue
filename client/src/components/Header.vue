3218f12c862c9e1f2f941b68ab793b2429dcea88
kenwoo9y <kenwoo9y@gmail.com>
2024-05-13 23:57:15 +0900

    add Header

---
+++ client/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <v-app-bar color="primary" dense dark>
+        <v-toolbar-title>todo app</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <AvatarMenu />
+      </v-app-bar>
+    </div>
+</template>
