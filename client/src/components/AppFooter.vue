099c9e07b8c8af4cb9d97d1fcf033f71404e07fa
kenwoo9y <kenwoo9y@gmail.com>
2024-05-18 22:30:03 +0900

    Added AppFooter

---
+++ client/src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-footer app :elevation="5" color="primary" class="d-flex align-center justify-center">
+        <div>
+            {{ `© ${copyright.date} ${copyright.name}` }}
+        </div>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        copyright: {
+            date: "2024",
+            name: "kenwoo9y",
+        }
+    })
+}
+</script>
