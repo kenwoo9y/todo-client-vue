8189e944bc6bd0ef132b55ec874b5667362721ff
kenwoo9y <kenwoo9y@gmail.com>
2024-05-18 22:29:42 +0900

    Added AppHeader

---
+++ client/src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app-bar :elevation="5" color="primary">
+        <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+      
+        <v-app-bar-title>{{ appName }}</v-app-bar-title>
+
+        <template v-slot:append>
+            <v-btn>
+                <v-icon :icon="mdiLogin" />
+            </v-btn>
+        </template>
+    </v-app-bar>
+</template>
+
+<script>
+import { mdiLogin } from '@mdi/js'
+
+export default {
+    data: () => ({
+        appName: "ToDo List",
+        mdiLogin
+    })
+}
+</script>
--- client/src/components/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-      <v-app-bar color="primary" dense dark>
-        <v-toolbar-title>todo app</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <AvatarMenu />
-      </v-app-bar>
-    </div>
-</template>
