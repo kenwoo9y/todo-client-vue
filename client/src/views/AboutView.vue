7b0dd9b3947deee761d111fe19e808a10d454ea7
kenwoo9y <kenwoo9y@gmail.com>
2024-05-23 23:36:23 +0900

    Add AboutView

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+    <Footer />
+    <Header />
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+</template>
+
+<script setup>
+import Header from '@/components/AppHeader.vue'
+import Footer from '@/components/AppFooter.vue'
+</script>
