4314f3d47d2a0f1c54819b1a3d2b19fdb6387e08
kenwoo9y <kenwoo9y@gmail.com>
2024-05-24 21:49:05 +0900

    Add icons and links to menus

--- client/src/components/NavigationDrawers.vue
+++ client/src/components/NavigationDrawers.vue
@@ -1,12 +1,29 @@
 <template>
     <v-navigation-drawer v-model="model">
         <v-list nav>
-            <v-list-item title="Home" value="home"></v-list-item>
-            <v-list-item title="About" value="about"></v-list-item>
+            <v-list-item 
+                v-for="item in items" 
+                :key="item.title" 
+                :to="item.link"
+            >
+                <template v-slot:prepend>
+                    <v-icon :icon="item.icon"></v-icon>
+                </template>
+
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
         </v-list>
     </v-navigation-drawer>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { mdiHome } from '@mdi/js'
+import { mdiForum } from '@mdi/js'
+
 const model = defineModel()
+const items = ref([
+    {title: 'Home', icon: mdiHome, link: {name: 'home'}},
+    {title: 'About', icon: mdiForum, link: {name: 'about'}}
+])
 </script>
