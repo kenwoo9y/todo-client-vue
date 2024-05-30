5cccfb63d50bb9c51cfa5545abef9f6b1ce07ee2
kenwoo9y <kenwoo9y@gmail.com>
2024-05-30 20:20:54 +0900

    Add TaskCreate dialog

--- client/src/components/TaskCreate.vue
+++ client/src/components/TaskCreate.vue
@@ -1,11 +1,55 @@
 <template>
-    <v-btn color="primary" variant="outlined" class="text-capitalize">
-        {{ buttonText }}
+    <v-btn color="primary" variant="outlined" class="text-capitalize" @click="openDialog">
+        {{ addButtonText }}
     </v-btn>
+
+    <v-dialog v-model="dialog" max-width="800">
+        <v-sheet>
+            <v-card title="New Task"></v-card>
+            <v-sheet class="my-2 mx-5">
+                <v-text-field 
+                    label="Title*"
+                    required
+                ></v-text-field>
+
+                <v-textarea 
+                    v-model="text" 
+                    label="Description"
+                ></v-textarea>
+
+                <v-select 
+                    :items=statusList
+                    label="Status*"
+                    required
+                ></v-select>
+
+                <div class="d-flex justify-end my-2">
+                    <v-btn class="mx-2 text-capitalize" color="primary" @click="handleClick(true)">
+                        {{ registerButtonText }}
+                    </v-btn>
+                    <v-btn class="mx-2 text-capitalize" variant="outlined" @click="handleClick(false)">
+                        {{ cancelButtonText }}
+                    </v-btn>
+                </div>
+            </v-sheet>
+        </v-sheet>
+    </v-dialog>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-const buttonText = ref("Add Task")
+const dialog = ref(false);
+const addButtonText = ref("Add Task");
+const registerButtonText = ref("Register Now");
+const cancelButtonText = ref("Cancel");
+const statusList = ref(['ToDo', 'Doing', 'Done'])
+
+const openDialog = () => {
+    dialog.value = true;
+};
+
+const handleClick = () => {
+    dialog.value = false;
+};
 </script>
