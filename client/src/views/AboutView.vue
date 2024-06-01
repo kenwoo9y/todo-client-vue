9bed98996c00572015326e8ca143a4f9fcdb7d02
kenwoo9y <kenwoo9y@gmail.com>
2024-06-01 23:57:01 +0900

    fix About Page

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,9 +1,19 @@
 <template>
     <Footer />
     <Header />
-    <div class="about">
-      <h1>This is an about page</h1>
-    </div>
+    <v-container class="about-page" fluid>
+        <v-row>
+            <v-col cols="12" class="text-center">
+                <v-divider></v-divider>
+                <h1 class="my-4">About Us</h1>
+                <p>
+                    Welcome to our website! We are dedicated to providing the best service
+                    to our customers. Our team is passionate and committed to excellence.
+                </p>
+                <v-divider></v-divider>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup>
