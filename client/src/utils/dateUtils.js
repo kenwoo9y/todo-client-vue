8035b373ab239b1cc6f298ef718a48d5c312ac41
kenwoo9y <kenwoo9y@gmail.com>
2024-06-25 00:05:46 +0900

    Add formatDateTime

--- client/src/utils/dateUtils.js
+++ client/src/utils/dateUtils.js
@@ -22,3 +22,20 @@ export function formatDate(dateString) {
 
     return formattedDate;
 }
+
+export function formatDateTime(originalDateTime) {
+    // 入力された文字列をDateオブジェクトに変換
+    const date = new Date(originalDateTime);
+
+    // 年、月、日、時、分を取得
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // 月は0から始まるため+1し、2桁にパディング
+    const day = String(date.getDate()).padStart(2, '0'); // 日を2桁にパディング
+    const hours = String(date.getHours()).padStart(2, '0'); // 時を2桁にパディング
+    const minutes = String(date.getMinutes()).padStart(2, '0'); // 分を2桁にパディング
+
+    // フォーマットを整えて返す
+    const formattedDateTime = `${year}/${month}/${day} ${hours}:${minutes}`;
+
+    return formattedDateTime;
+}
