a31e936740da0417aeb334a2836aaa2c24056ef1
kenwoo9y <kenwoo9y@gmail.com>
2025-03-07 21:16:42 +0900

    fix formatDateTime

--- client/src/utils/dateUtils.js
+++ client/src/utils/dateUtils.js
@@ -24,21 +24,26 @@ export function formatDate(dateString) {
 }
 
 export function formatDateTime(originalDateTime) {
+    // originalDateTime が null、undefined、または空文字の場合は空文字を返す
+    if (!originalDateTime) return '';
+
     // 入力された文字列をDateオブジェクトに変換
     const date = new Date(originalDateTime);
 
-    // 日本標準時 (JST) にするために9時間を加算
-    date.setHours(date.getHours() + 9);
+    // 日本時間に変換
+    const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Asia/Tokyo', // 明示的にタイムゾーンを指定
+    };
 
-    // 年、月、日、時、分を取得
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // 月は0から始まるため+1し、2桁にパディング
-    const day = String(date.getDate()).padStart(2, '0'); // 日を2桁にパディング
-    const hours = String(date.getHours()).padStart(2, '0'); // 時を2桁にパディング
-    const minutes = String(date.getMinutes()).padStart(2, '0'); // 分を2桁にパディング
-
-    // フォーマットを整えて返す
-    const formattedDateTime = `${year}/${month}/${day} ${hours}:${minutes}`;
+    const formattedDateTime = date
+    .toLocaleDateString('ja-JP', options)
+    .replace(/\//g, '-')
+    .replace(/\s/g, ' ');
 
     return formattedDateTime;
 }
